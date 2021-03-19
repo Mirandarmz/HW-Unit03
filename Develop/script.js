199aa2bccfb9ac3fc9bdde80fc241e4c09809090
mirandarmz <mirandarmz.c@gmail.com>
2021-03-19 10:28:24 -0600

    Solved limitation, included comments and readme

--- Develop/script.js
+++ Develop/script.js
@@ -72,96 +72,41 @@ function generatePassword(){
 
   }
   
-  return password;
-}
-
-
-
-function generatePassword2(){
-  var upCase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var lowCase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var specCar=['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','>','<','=','?','@','[',']','^','-','{','}','|',];
-  var numb=['1','2','3','4','5','6','7','8','9'];
-
-  var password=""; //we initialize the variable password that will laster be used to concatenate the elements
-  var numbCar=window.prompt("How many characters would you like your pasword to have?");
-
-  if(numbCar<8){//if the number of characters selected is smaller than 8, it will ask to introduce the value again
-    window.alert("Password must be at least 8 characters");
-    numbCar=window.prompt("How many characters would you like your pasword to have?");
-  }
-
-  //saving in a variable which elements does the user want to be contained in the password, all booleans
-  var containSpecialCar=window.confirm("Click OK if you want password to have special characters");
-  var containNumCar=window.confirm("Click OK if you want password to have numeric characters");
-  var containLowCase=window.confirm("Click OK if you want password to have lower case characters");
-
- 
-  var random;
+  password=randomize(password);//to eliminate the conistent pattern (special character, then lower case, then number, they upper case), a randomize function was made to swap the elements in the password at random
   
-  if(containSpecialCar && containNumCar && containLowCase){ //si tiene special car y number y low cases
-    var allCar=upCase.concat(lowCase,specCar,numb);
-    while(password.length<numbCar){
-      random=Math.floor(Math.random()*allCar.length);
-      password=password.concat(allCar[random]);
-    }
-  }else{
-    if(containSpecialCar && containNumCar){ //si tiene special car y num
-      var carAndNumb=upCase.concat(specCar,numb);
-      while(password.length<numbCar){
-        random=Math.floor(Math.random()*carAndNumb.length);
-        password=password.concat(carAndNumb[random]);
-      }
-    }else{
-      if(containSpecialCar && containLowCase){//si tiene special car y low case
-        var carAndLow=upCase.concat(lowCase,specCar);
-        while(password.length<numbCar){
-          random=Math.floor(Math.random()*carAndLow.length);
-          password=password.concat(carAndLow[random]);
-        } 
-      }else{
-        if(containLowCase && containNumCar){//si tiene low case y number
-          var lowAndNum=upCase.concat(lowCase,numb);
-          while(password.length<numbCar){
-            random=Math.floor(Math.random()*lowAndNum.length);
-            password=password.concat(lowAndNum[random]);
-          } 
-        }else{
-          if(containLowCase){//si solo tiene low case
-            while(password.length<numbCar){
-              random=Math.floor(Math.random()*upCase.concat(lowCase).length);
-              password=password.concat(upCase.concat(lowCase)[random]);
-            } 
-          }
-          else{
-            if(containNumCar){ //si solo tiene number
-              while(password.length<numbCar){
-                random=Math.floor(Math.random()*upCase.concat(numb).length);
-                password=password.concat(upCase.concat(num)[random]);
-              } 
-            }else{
-              if(containSpecialCar){ //si solo tiene special car
-                while(password.length<numbCar){
-                  random=Math.floor(Math.random()*upCase.concat(specCar).length);
-                  password=password.concat(upCase.concat(specCar)[random]);
-                } 
-              }else{ //si no tiene ninguno
-                while(password.length<numbCar){
-                  random=Math.floor(Math.random()*upCase.length);
-                  password=password.concat(upCase[random]);
-                } 
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-
   return password;
-
 }
 
+//Made an additional function to randomly swap the elements of the password and solve the consistent pattern problem 
+function randomize(text){
+  var textArray=[];
+
+  //first turn the string into an array
+  for(var i=0;i<text.length;i++){
+    textArray[i]=text.charAt(i);
+  }
+  
+  var aux;
+  var random;
+
+  //For the lenght of the array, every element is randomly switched 
+  for(var i=0;i<text.length;i++){
+    random=Math.floor(Math.random()*textArray.length);
+    aux=textArray[random];
+    textArray[random]=textArray[i];
+    textArray[i]=aux;
+  }
+
+  var resp="";
+
+  //turn the array into a string
+  for(var i=0;i<text.length;i++){
+    resp=resp.concat(textArray[i]);
+  }
+
+  //return the mixed string
+  return resp;
+}
  
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
